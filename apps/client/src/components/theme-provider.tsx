4bcea09a776ac2628329f3a06617694959d07b69
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-02-05 20:04:10 +0530

    feat: made a theme provider for dark mode

---
+++ apps/client/src/components/theme-provider.tsx
@@ -0,0 +1,47 @@
+import React, { createContext, useEffect, useState } from "react";
+
+type ThemeType = "light" | "dark" | "system";
+export const themeContext = createContext({
+  theme: "",
+  toggleTheme: () => {},
+});
+export default function ThemeProvider({
+	children,
+	defaultTheme,
+}: {
+	children: React.ReactNode;
+	defaultTheme?: "light" | "dark" | "system";
+}) {
+	const [theme, setTheme] = useState<ThemeType>(
+		defaultTheme === "system"
+			? window.matchMedia("(prefers-color-scheme: dark)").matches
+				? "dark"
+				: "light"
+			: defaultTheme || "dark",
+	);
+
+	useEffect(() => {
+		const saved = localStorage.getItem("theme") as ThemeType;
+		const prefersDark = window.matchMedia(
+			"(prefers-color-scheme: dark)",
+		).matches;
+		const finalTheme: ThemeType = saved || (prefersDark ? "dark" : "light");
+
+		setTheme(finalTheme);
+		document.querySelector("html")?.classList.add(finalTheme);
+	}, []);
+
+	const toggleTheme = () => {
+		const next = theme === "light" ? "dark" : "light";
+		setTheme(next);
+		document.querySelector("html")?.classList.remove(theme);
+		document.querySelector("html")?.classList.add(next);
+		localStorage.setItem("theme", next);
+	};
+
+  return (
+    <themeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </themeContext.Provider>
+  );
+}
