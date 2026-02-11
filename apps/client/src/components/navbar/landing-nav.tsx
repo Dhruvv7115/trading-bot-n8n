40dee6100515138e03fae0ca94636d4925fe9cb1
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2026-02-11 07:44:55 +0530

    refactor: add on click handlers on signup and signin buttons

--- apps/client/src/components/navbar/landing-nav.tsx
+++ apps/client/src/components/navbar/landing-nav.tsx
@@ -3,12 +3,14 @@ import { motion } from "motion/react";
 import React, { useContext } from "react";
 import { themeContext } from "../theme-provider";
 import { Button } from "../ui/button";
+import { useNavigate } from "react-router-dom";
 
 export default function LandingNav() {
 	const { theme, toggleTheme } = useContext(themeContext);
+	const navigate = useNavigate();
 	return (
 		<nav className="mx-auto max-w-5xl fixed inset-x-0 md:top-4 top-0 z-50">
-      {/* web navbar */}
+			{/* web navbar */}
 			<div className="hidden md:flex items-center justify-between w-full px-6 py-4 gap-4 border border-border bg-background/20 backdrop-blur-md rounded-lg">
 				<div className="flex items-center gap-2 font-semibold text-xl text-foreground">
 					<div className="p-2 bg-primary text-primary-foreground rounded-sm rotate-90 shadow-sm">
@@ -30,24 +32,40 @@ export default function LandingNav() {
 					</motion.button>
 
 					<div className="flex items-center gap-2">
-						<Button className="rounded-[12px]">Sign In</Button>
-						<Button className="rounded-[12px]" variant="outline">Get Started</Button>
+						<Button
+							onClick={() => navigate("/signin")}
+							className="rounded-[12px]"
+						>
+							Sign In
+						</Button>
+						<Button
+							onClick={() => navigate("/signup")}
+							className="rounded-[12px]"
+							variant="outline"
+						>
+							Get Started
+						</Button>
 					</div>
 				</div>
 			</div>
-      {/* mobile navbar */}
-      <div className="flex items-center justify-between w-full px-6 py-4 gap-4 md:hidden border-y border-border bg-background/80 backdrop-blur-xl">
-        <div className="flex items-center gap-2 font-semibold text-xl text-foreground">
-          <div className="p-2 bg-primary text-primary-foreground rounded-sm rotate-90 shadow-sm">
-            <GitFork className="w-5 h-5" />
-          </div>
-          <h2>TradeFlow</h2>
-        </div>
-        <div className="flex items-center gap-2">
-          <Button>Sign In</Button>
-          <Button variant="outline">Get Started</Button>
-        </div>
-      </div>
+			{/* mobile navbar */}
+			<div className="flex items-center justify-between w-full px-6 py-4 gap-4 md:hidden border-y border-border bg-background/80 backdrop-blur-xl">
+				<div className="flex items-center gap-2 font-semibold text-xl text-foreground">
+					<div className="p-2 bg-primary text-primary-foreground rounded-sm rotate-90 shadow-sm">
+						<GitFork className="w-5 h-5" />
+					</div>
+					<h2>TradeFlow</h2>
+				</div>
+				<div className="flex items-center gap-2">
+					<Button onClick={() => navigate("/signin")}>Sign In</Button>
+					<Button
+						onClick={() => navigate("/signup")}
+						variant="outline"
+					>
+						Get Started
+					</Button>
+				</div>
+			</div>
 		</nav>
 	);
 }
--- apps/client/src/pages/Landing.tsx
+++ apps/client/src/pages/Landing.tsx
@@ -1,12 +1,8 @@
-import { Link } from "react-router-dom";
-import { ArrowRight, Bot, Zap, Shield } from "lucide-react";
 import LandingNav from "@/components/navbar/landing-nav";
 import Container from "@/components/container";
-import FeatureCard from "@/components/feature-card";
 import LandingHero from "@/components/landing-hero";
 
 export default function Landing() {
-	// const { theme, toggleTheme } = useContext(themeContext);
 	return (
 		<Container>
 			<div className="min-h-screen bg-background text-foreground selection:bg-primary/20">
